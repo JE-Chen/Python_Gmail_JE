25e162c49d58a5f290130a13f3c618c4fbe44c39
JE-Chen <zenmailman@gmail.com>
2020-12-14 10:17:40 +0800

    refactor
    refactor

--- Test/UnitTest.py
+++ Test/UnitTest.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import JEGmail
@@ -12,10 +13,11 @@ class TestGmail(unittest.TestCase):
         pass
 
     def test_log(self):
-        with open('/Templates/Email_Template1_Picture.html', 'r+') as File:
+        with open(os.getcwd() + '/Templates/Email_Template1_Picture.html', 'r+') as File:
             content = (File.read())
         self.Gmail.Gmail_API.send_mail_attach("410877027@mail.nknu.edu.tw", "410877027@mail.nknu.edu.tw", "Hello",
-                                              content, attach_file='/images/firefox_test.png', use_html=True)
+                                              content, attach_file=os.getcwd() + '/images/firefox_test.png',
+                                              use_html=True)
         File.close()
 
 

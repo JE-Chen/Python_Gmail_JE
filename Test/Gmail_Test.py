ac046941a68f97a6c402fc78f71d7bac2cbd77b7
JE-Chen <zenmailman@gmail.com>
2020-12-14 10:13:59 +0800

    refactor
    refactor

--- Test/Gmail_Test.py
+++ Test/Gmail_Test.py
@@ -1,8 +1,8 @@
 import JEGmail
 
 a = JEGmail.Core.GmailCore()
-with open('Templates/Email_Template1_Picture.html', 'r+') as File:
+with open('/Templates/Email_Template1_Picture.html', 'r+') as File:
     content = (File.read())
 a.Gmail_API.send_mail_attach("410877027@mail.nknu.edu.tw", "410877027@mail.nknu.edu.tw", "Hello",
-                             content, attach_file='images/firefox_test.png', use_html=True)
+                             content, attach_file='/images/firefox_test.png', use_html=True)
 File.close()

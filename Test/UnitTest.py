1b7aa2f22ce8fbc24212b732ef9b4767de98481b
JE-Chen <zenmailman@gmail.com>
2021-05-09 05:28:19 +0800

    edit unit test

--- Test/UnitTest.py
+++ Test/UnitTest.py
@@ -1,13 +1,13 @@
 import os
 import unittest
 
-import JEGmail
+from JEGmail.Core import GmailCore
 
 
 class TestGmail(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.Gmail = JEGmail.Core.GmailCore('/Test')
+        self.Gmail = GmailCore('/Test')
 
     def test_log(self):
         with open(os.getcwd() + '/Test/Templates/Email_Template1_Picture.html', 'r+') as File:
